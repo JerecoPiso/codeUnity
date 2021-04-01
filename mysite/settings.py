1fbbdba913a4e40cece606b03385d0c9793b4f59
JerecoPiso <jerecojamespiso@gmail.com>
2021-04-01 17:23:23 +0800

    df

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/statics/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/static/'
