f8cd9f658d1d75e23fb3c35f5dd97db061c90e4a
JerecoPiso <jerecojamespiso@gmail.com>
2021-08-04 16:30:43 +0800

    ff

--- mysite/settings.py
+++ mysite/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 # 587
 EMAIL_HOST_USER = 'jamesjerecopiso@gmail.com'
 EMAIL_HOST_PASSWORD = "PHPprogrammer20"
