b48a0684e84918b0fef9ebd42d9f61a47b6dd2a1
JerecoPiso <jerecojamespiso@gmail.com>
2021-08-04 16:23:55 +0800

    ff

--- mysite/settings.py
+++ mysite/settings.py
@@ -152,8 +152,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_PORT = 587
+# EMAIL_USE_SSL = False
+EMAIL_PORT = 465
+# 587
 EMAIL_HOST_USER = 'jamesjerecopiso@gmail.com'
 EMAIL_HOST_PASSWORD = "PHPprogrammer20"
 
