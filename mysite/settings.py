87861a3ce94bb6abb68ad2ebcd87ba4ad102f8e3
JerecoPiso <jerecojamespiso@gmail.com>
2021-08-04 17:16:37 +0800

    ff

--- mysite/settings.py
+++ mysite/settings.py
@@ -150,12 +150,13 @@ USE_TZ = True
 # SESSION_COOKIE_AGE = 20
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_PORT = 465
-# 587
+EMAIL_USE_TLS =True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+# 465
 EMAIL_HOST_USER = 'jamesjerecopiso@gmail.com'
 EMAIL_HOST_PASSWORD = "PHPprogrammer20"
 
